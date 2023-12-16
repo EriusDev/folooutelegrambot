2ebe01cfa87e96c19061562aa5626dcd059775f6
JULIO <julio@midiiia.com>
2023-12-17 00:57:48 +0100

    RENDER3

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-const telegramToken = process.env.TELEGRAM_TOKEN || '6901826067:AAH8LDhATAkzNVzYKkqEigS_pKXMpX6u2LE';
+const telegramToken = process.env.TELEGRAM_TOKEN || 'TU_TOKEN_AQUI';
 const bot = new TelegramBot(telegramToken, { polling: false });
 
-// Obtén la URL única de Render y configura el Webhook
-const renderAppURL = 'https://folooutelegrambot.onrender.com'; // Reemplaza con tu URL de Render
+const renderAppURL = 'https://tubotenrender.onrender.com';
 const webhookPath = `/bot${telegramToken}`;
 
-// Configura el Webhook
 bot.setWebHook(`${renderAppURL}${webhookPath}`);
+
+// Variable para almacenar los datos recogidos
+const userData = {};
 
 // Manejar el comando /start con un teclado en línea
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const keyboard = {
     inline_keyboard: [
-      [{ text: 'Enviar nueva indicación', callback_data: 'new_instruction' }],
+      [{ text: 'Enviar indicaciones', callback_data: 'new_instruction' }],
       [{ text: 'Salir', callback_data: 'exit' }]
     ]
   };
@@ -36,8 +37,9 @@ bot.on('callback_query', (callbackQuery) => {
 
   switch (data) {
     case 'new_instruction':
-      bot.sendMessage(chatId, 'Por favor, envía tu nombre:');
-      // Puedes seguir con más preguntas...
+      // Inicia el proceso de recogida de datos
+      userData[chatId] = {};
+      bot.sendMessage(chatId, 'Por favor, envía el nombre de la indicación:');
       break;
     case 'exit':
       bot.sendMessage(chatId, 'Has seleccionado salir. ¡Hasta luego!');
@@ -52,15 +54,45 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  // Puedes agregar lógica aquí para manejar las respuestas a tus preguntas
-  switch (text) {
-    case 'Tu nombre es:':
-      // Lógica para manejar el nombre
-      bot.sendMessage(chatId, `¡Hola, ${text}! Ahora, por favor, envía tu correo electrónico.`);
-      break;
-    // Agrega más casos según sea necesario...
-    default:
-      bot.sendMessage(chatId, 'Por favor, sigue las instrucciones.');
+  // Verificar si hay un proceso de recogida de datos en curso para este usuario
+  if (userData[chatId]) {
+    const userDataChat = userData[chatId];
+
+    // Manejar cada paso de la recogida de datos
+    if (!userDataChat.nombre) {
+      userDataChat.nombre = text;
+      bot.sendMessage(chatId, 'Ahora, por favor, envía la localización:');
+    } else if (!userDataChat.localizacion) {
+      // Puedes manejar la localización de manera especial si es necesario
+      // userDataChat.localizacion = ...
+      bot.sendMessage(chatId, 'Ahora, por favor, envía la descripción (opcional):');
+    } else if (!userDataChat.descripcion) {
+      userDataChat.descripcion = text;
+      bot.sendMessage(chatId, 'Por último, envía un video o audio:');
+    } else if (!userDataChat.media) {
+      // Puedes manejar el video o audio de manera especial si es necesario
+      // userDataChat.media = ...
+      bot.sendMessage(chatId, 'Resumen de la indicación:\n\n' +
+        `Nombre: ${userDataChat.nombre}\n` +
+        `Localización: ${userDataChat.localizacion}\n` +
+        `Descripción: ${userDataChat.descripcion || 'No proporcionada'}\n` +
+        `Video o Audio: ${userDataChat.media ? 'Adjunto' : 'No proporcionado'}\n\n` +
+        '¿Todo está correcto?',
+        { reply_markup: { inline_keyboard: [[{ text: 'Sí', callback_data: 'confirm' }]] } }
+      );
+    }
+  }
+});
+
+// Manejar la confirmación del resumen
+bot.on('callback_query', (callbackQuery) => {
+  const chatId = callbackQuery.message.chat.id;
+  const data = callbackQuery.data;
+
+  if (data === 'confirm' && userData[chatId]) {
+    bot.sendMessage(chatId, '¡Gracias por enviar las indicaciones! Proceso completado.');
+    // Puedes hacer lo que quieras con los datos recopilados aquí
+    delete userData[chatId]; // Limpiar los datos después de completar el proceso
   }
 });
 
